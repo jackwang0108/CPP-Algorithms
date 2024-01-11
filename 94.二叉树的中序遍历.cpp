750544e4e9621aef88a630ba3dcd4616dc7ffb05
Jack <jack4shihong@gmail.com>
2024-01-11 17:31:04 +0800

    add inorder traversal

---
+++ 94.二叉树的中序遍历.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+#include <stack>
+#include <vector>
+
+using std::stack;
+using std::vector;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    // 递归解法, 很简单
+    vector<int> inorderTraversal1(TreeNode *root)
+    {
+        if (root == nullptr)
+            return {};
+
+        vector<int> left = inorderTraversal1(root->left);
+        left.insert(left.end(), root->val);
+        vector<int> right = inorderTraversal1(root->right);
+        left.insert(left.end(), right.cbegin(), right.cend());
+        return left;
+    }
+
+    // 高效版, 使用引用避免复制
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        inorderTraverser(root, ans);
+        return ans;
+    }
+
+    void inorderTraverser(TreeNode *root, vector<int> &ans)
+    {
+        if (root == nullptr)
+            return;
+        inorderTraverser(root->left, ans);
+        ans.insert(ans.end(), root->val);
+        inorderTraverser(root->right, ans);
+    }
+};
+// @lc code=end
+
+class Solution
+{
+    // 非递归解法, 需要使用栈
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        stack<TreeNode *> st;
+        while (root != nullptr || !st.empty())
+        {
+            // 访问左子树
+            while (root != nullptr)
+            {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            ans.insert(ans.end(), root->val);
+            root = root->right;
+        }
+        return ans;
+    }
+};
